97ae7adaf7eb26b99affdbb04c4aa8ce53a7c775
Raicley Santana <raicleysantana1@gmail.com>
2021-03-29 16:02:55 -0400

    Atualizações na pagina de login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {AppBar, IconButton, Toolbar, Typography, Button, Container, Card, CardContent} from '@material-ui/core';
+import {
+    AppBar,
+    IconButton,
+    Toolbar,
+    Typography,
+    Button,
+    Container,
+    Card,
+    CardContent,
+    Grid,
+    TextField
+} from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import './style.css';
+import logo from '../../assets/images/logo.png';
 
 function Login() {
     return (
@@ -23,7 +35,50 @@ function Login() {
             <div id="login-content">
                 <Card>
                     <CardContent>
-                        oi
+                        <Grid
+                            container
+                            direction="column"
+                            justify="center"
+                            alignItems="center"
+                        >
+                            <Grid item md={12}>
+                                <img src={logo} className="login-logo"/>
+                            </Grid>
+                            <Grid item md={12}>
+                                <Typography variant="h5">Acessar o Sistema</Typography>
+                            </Grid>
+                            <br/>
+                            <Grid item md={12}>
+                                <TextField
+                                    id="form-cpf"
+                                    label="CPF"
+                                    style={{margin: 8}}
+                                    margin="normal"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="outlined"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="form-senha"
+                                    label="Senha"
+                                    style={{margin: 8}}
+                                    margin="normal"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="outlined"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <Button className="button" variant="contained">Entrar</Button>
+                            </Grid>
+                        </Grid>
+
                     </CardContent>
                 </Card>
             </div>
