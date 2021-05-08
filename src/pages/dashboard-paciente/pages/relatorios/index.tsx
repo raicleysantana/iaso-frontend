c01964966ef81753fe9f7eaa5eb170700862d41f
raicleysantana <raicleysantana1@gmail.com>
2021-05-08 02:20:41 -0400

    Andamento de telas de relatorios no dashboard do paciente

--- src/pages/dashboard-paciente/pages/relatorios/index.tsx
+++ src/pages/dashboard-paciente/pages/relatorios/index.tsx
@@ -1,44 +1,47 @@
 import React from 'react';
 import {DataGrid, GridColDef, GridValueFormatterParams} from '@material-ui/data-grid';
+import {Button} from "@material-ui/core";
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import './style.css';
 
 const rows = [
-    {id: 1, lastName: 'Snow', firstName: 'Jon', age: 35},
-    {id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42},
-    {id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45},
-    {id: 4, lastName: 'Stark', firstName: 'Arya', age: 16},
-    {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null},
-    {id: 6, lastName: 'Melisandre', firstName: null, age: 150},
-    {id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44},
-    {id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36},
-    {id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65},
+    {id: 1, dataConsulta: '20-05-2021', firstName: 'Jon', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 2, dataConsulta: '20-05-2021', firstName: 'Cersei', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 3, dataConsulta: '20-05-2021', firstName: 'Jaime', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 4, dataConsulta: '20-05-2021', firstName: 'Arya', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 5, dataConsulta: '20-05-2021', firstName: 'Daenerys', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 6, dataConsulta: '20-05-2021', firstName: null, dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 7, dataConsulta: '20-05-2021', firstName: 'Ferrara', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 8, dataConsulta: '20-05-2021', firstName: 'Rossini', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
+    {id: 9, dataConsulta: '20-05-2021', firstName: 'Harvey', dataNascimento: 'dd/mm/yyyy', situacao: 'Compareceu'},
 ];
 
 const columns: GridColDef[] = [
     {field: 'id', headerName: 'ID', width: 70},
-    {field: 'firstName', headerName: 'First name', width: 130},
-    {field: 'lastName', headerName: 'Last name', width: 130},
+    {field: 'dataConsulta', headerName: 'Data consulta', width: 150},
+    {field: 'firstName', headerName: 'Nome do Paciente', width: 250},
+    {field: 'dataNascimento', headerName: 'Data de Nascimento', width: 130},
+    {field: 'situacao', headerName: 'Situação', width: 130},
     {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params: GridValueFormatterParams) =>
-            `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+        field: 'teste',
+        width: 170,
+        headerName: 'Relatórios',
+        renderCell: (params: GridValueFormatterParams) => {
+            return (<Button className={"btn-visualizar"}><VisibilityIcon style={{ width : 20, marginRight : 5}}/>  Visualizar</Button>);
+        }
     },
 ];
 
 function Relatorios() {
     return (
-        <div>
+        <div style={{backgroundColor: '#FFFFFF'}}>
             <div style={{height: 400, width: '100%'}}>
-                <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection/>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    checkboxSelection
+                />
             </div>
         </div>
     );
