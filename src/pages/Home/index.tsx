2f8bc05a142c7546bc544fb82d61831bfcbbb345
raicleysantana <raicleysantana1@gmail.com>
2021-05-24 20:45:26 -0400

    TESTE fetch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {AppBar, Toolbar} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 
 function Home() {
+
+	useEffect(() => {
+		fetch("https://iaso-api.000webhostapp.com", {
+      		method: 'GET',
+      		headers: {
+        		'Content-Type': 'application/json',
+        		"Access-Control-Allow-Origin": "*"
+      		},
+      //body: JSON.stringify({ produto })
+    })
+		.then((response) => alert(JSON.stringify(response.json()))).then((responseJson) => {
+        console.log(responseJson)
+     	
+      })
+	},[]);
+
     return (<>
 
             <AppBar position="fixed" color="inherit">
